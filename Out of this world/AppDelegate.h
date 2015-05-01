040d5aea5b9d54a296fe642a7b972d2242072718
mateoencar <encar7435@gapps.uwcsea.edu.sg>
2015-05-01 18:53:32 +0800

    Initial Commit

---
+++ Out of this world/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this world
+//
+//  Created by mateo on 1/5/15.
+//  Copyright (c) 2015 mateo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this world/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Out of this world
+//
+//  Created by mateo on 1/5/15.
+//  Copyright (c) 2015 mateo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
